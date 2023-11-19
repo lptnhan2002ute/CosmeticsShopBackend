8dc88c5cfd22aff08376d47a9ac9f4624834e7b6
lptnhan2002ute <20110688@student.hcmute.edu.vn>
2023-11-20 00:38:23 +0700

    update

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -8,6 +8,7 @@ const sendMail = require('../ultils/sendMails')
 const crypto = require('crypto')
 const bcrypt = require('bcrypt')
 const createToken = require('uniqid')
+const createToken = require('uniqid')
 
 // Register for Guest
 // const registerGuest = asyncHandler(async (req, res) => {
