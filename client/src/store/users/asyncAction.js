10a354d3ffc021331e21932b2c08daedf9a24f75
lptnhan2002ute <20110688@student.hcmute.edu.vn>
2023-11-24 14:59:10 +0700

    update nhỏ

--- client/src/store/users/asyncAction.js
+++ client/src/store/users/asyncAction.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import * as apis from '../../apis'
-export const getCurrent = createAsyncThunk('user/customer', async (data, { rejectWithValue }) => {
+export const getCurrent = createAsyncThunk('user/current', async (data, { rejectWithValue }) => {
     const response = await apis.apiGetCurrent()
 
     if (!response.success) return rejectWithValue(response)
