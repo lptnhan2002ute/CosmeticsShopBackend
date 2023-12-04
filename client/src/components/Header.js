15b84e143e046912b163c2f88b70b9e4167abea8
nquy123 <114900285+nquy123@users.noreply.github.com>
2023-12-04 12:09:16 +0700

    quy update

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, {Fragment, useState, useEffect} from 'react'
 import logo from '../assets/logo.svg.png'
 import icons from '../ultils/icon'
 import { Link } from 'react-router-dom'
 import path from '../ultils/path'
+import { useSelector, useDispatch } from 'react-redux'
+import { logout } from '../store/users/userSlice'
 
 const Header = () => {
 const {RiPhoneFill, MdEmail, BiUserCircle, FaShoppingBag} = icons
+const {current} = useSelector(state => state.user)
+const [isShowOption, setIsShowOption] = useState(false)
+const dispatch = useDispatch()
+useEffect(() => {
+    const handleClickout = (e) => {
+        const profile = document.getElementById('profile')
+        if (!profile?.contains(e.target)) setIsShowOption(false)
+
+    }
+    document.addEventListener('click', handleClickout)
+    return () => {
+        document.removeEventListener('click', handleClickout)
+    }
+}, [])
     return (
         <div className='w-main flex justify-between h-[120px] py-[35px]'>
             <Link to={`/${path.HOME}`}>
@@ -26,14 +42,31 @@ const {RiPhoneFill, MdEmail, BiUserCircle, FaShoppingBag} = icons
                     </span>
                     <span>Online Help 24/7</span>
                 </div>
-                <div className='cursor-pointer flex items-center justify-center gap-2 px-6 border-r'>
+                {current && <Fragment>
+                    <div className='cursor-pointer flex items-center justify-center gap-2 px-6 border-r'>
                     <FaShoppingBag color='#ff007f' />
                     <span>0 item(s)</span>
                 </div>
-                <div className='cursor-pointer flex items-center justify-center px-6'>
+                <div 
+                className='cursor-pointer flex items-center justify-center px-6 relative'
+                onClick={() => setIsShowOption(prev => !prev)}
+                id='profile'
+                >
                     <BiUserCircle color='#ff007f' size={24} />
                     <span>Profile</span>
+                    {isShowOption && <div
+                    onClick={e => e.stopPropagation()}
+                    className='flex flex-col absolute top-full left-[16px] bg-gray-100 border min-w-[150px] py-2'>
+                    <Link className='w-full p-2 hover:bg-sky-100' to={`/${path.MEMBER}/${path.PERSONAL}`}>Thông tin cá nhân
+                    </Link>
+                    {current?.role === 'Admin' && <Link className='w-full p-2 hover:bg-sky-100' to={`/${path.ADMIN}/${path.DASHBOARD}`}>Trang Quản Lí
+                    </Link>}
+                    <span 
+                    onClick={() => dispatch(logout())}
+                    className='w-full p-2 hover:bg-sky-100'>Đăng xuất</span>
+                    </div>}
                     </div>
+                </Fragment>}
             </div>
         </div>
     )
---
+++ client/src/pages/admin/CreateProduct.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateProduct = () => {
+    return (
+        <div>
+            AdminLayout
+        </div>
+    )
+}
+
+export default CreateProduct
