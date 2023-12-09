76e670867fb12ac41ef74e5328a3c55394d348ae
lptnhan2002ute <20110688@student.hcmute.edu.vn>
2023-12-10 00:03:43 +0700

    delete cartSlice

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,7 +2,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import appSlice from './appSlice';
 import productSlice from './products/productSlice';
 import userSlice from './users/userSlice';
-import cartSlice from './users/cartSlice';
 import storage from 'redux-persist/lib/storage';
 import {persistStore, persistReducer, FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE} from 'redux-persist'
 
@@ -16,18 +15,13 @@ const userConfig = {
   whitelist: ['isLoggedIn', 'token', 'current']
 }
 
-const cartConfig = {
-  ...commonConfig,
-  whitelist: ['isLoggedIn', 'token', 'cart']
-}
-
 
 export const store = configureStore({
   reducer: {
     app: appSlice,
     products: productSlice,
     user: persistReducer(userConfig, userSlice),
-    cart: persistReducer(cartConfig, cartSlice),
+  
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
