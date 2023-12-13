0cd1f8b952943e24bbae4276647b524ba206ab48
Danh Ho <thanhdanh.ute2002@gmail.com>
2023-12-14 03:01:45 +0700

    init docker

--- server/config/dbconnect.js
+++ server/config/dbconnect.js
@@ -1,14 +1,18 @@
- const {default: mongoose} = require('mongoose')
+const { default: mongoose } = require("mongoose");
 
- const dbConnect = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI)
-        if (conn.connection.readyState == 1) console.log('DB connection is ready to connect')
-        else console.log('Connection is not ready to connect')
-
-    } catch (error) {
-        console.log('DB connection error')
-        throw new Error(error)
-    }
- }
- module.exports = dbConnect 
+const dbConnect = async () => {
+  try {
+    console.log(process.env.MONGODB_URI)
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    if (conn.connection.readyState == 1)
+      console.log("DB connection is ready to connect");
+    else console.log("Connection is not ready to connect");
+  } catch (error) {
+    console.log("DB connection error");
+    throw new Error(error);
+  }
+};
+module.exports = dbConnect;
