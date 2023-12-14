ab94811c342898ba10f5270b36f434b7f72ac713
nquy123 <114900285+nquy123@users.noreply.github.com>
2023-12-14 11:12:37 +0700

    update dashboard

--- client/src/pages/admin/Dashboard.js
+++ client/src/pages/admin/Dashboard.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Dashboard = () => {
     return (
-        <div>
-            Dashboard
+        <div className='w-full'>
+            <h1 className='h-[75px] justify-between flex items-center text-3xl font-semiblod px-4 border-b border-b-main'>
+                <span>Chào mừng bạn đến với trang quản lí</span>
+            </h1>
+            <img className='w-full' src='https://cdn.tgdd.vn/hoi-dap/1374019/200-hinh-nen-slide-mo-dau-powerpoint-cuc-chuyen-nghiep-an181-1-800x450.jpg'
+            alt='anh' />
         </div>
     )
 }
