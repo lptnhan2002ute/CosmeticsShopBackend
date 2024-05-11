488abe289696cb1c272753485a56ab44d32eda00
lptnhan2002ute <20110688@student.hcmute.edu.vn>
2024-05-11 23:13:09 +0700

    update voucher controller

--- server/controllers/voucher.js
+++ server/controllers/voucher.js
@@ -1,6 +1,8 @@
 const Voucher = require('../models/voucher')
 const asyncHandler = require('express-async-handler')
 const moment = require('moment');
+const sendMail = require('../ultils/sendMails')
+const User = require('../models/user')
 
 const createVoucher = asyncHandler(async (req, res) => {
     const { name, discount, maxDiscountAmount, minPurchaseAmount, maxUsage, startDay, endDay } = req.body;
@@ -104,8 +106,33 @@ const findById = asyncHandler(async (req, res) => {
 })
 
 const getAllVouchers = asyncHandler(async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 5;
+    const { startDate, endDate, name } = req.query;
+
+    let query = {};
+    if (name) {
+        query.name = new RegExp(name, 'i'); // Sử dụng RegExp để tìm kiếm không phân biệt hoa thường
+    }
+
+    // Thêm điều kiện tìm kiếm theo ngày vào truy vấn
+    if (startDate || endDate) {
+        if (startDate) {
+            query.startDay = { $gte: moment(startDate, 'DD/MM/YYYY').startOf('day').toDate() };
+        }
+        if (endDate) {
+            query.endDay = { $lte: moment(endDate, 'DD/MM/YYYY').endOf('day').toDate() }; // Chỉnh sửa để sử dụng endDay
+        }
+    }
     try {
-        const vouchers = await Voucher.find().select('name _id logo minPurchaseAmount discount maxDiscountAmount usedCount maxUsage usedBy startDay endDay');
+        const total = await Voucher.countDocuments(query);
+        const totalPages = Math.ceil(total / limit);
+        const offset = (page - 1) * limit;
+
+        const vouchers = await Voucher.find(query)
+            .select('name _id logo minPurchaseAmount discount maxDiscountAmount usedCount maxUsage usedBy startDay endDay')
+            .skip(offset)
+            .limit(limit);
 
         if (!vouchers || vouchers.length === 0) {
             return res.status(404).json({
@@ -116,6 +143,9 @@ const getAllVouchers = asyncHandler(async (req, res) => {
 
         return res.status(200).json({
             success: true,
+            currentPage: page,
+            totalPages: totalPages,
+            totalItems: total,
             voucherList: vouchers
         });
     } catch (error) {
@@ -224,7 +254,7 @@ const deleteVoucher = asyncHandler(async (req, res) => {
 
 const checkVoucher = asyncHandler(async (req, res) => {
     const { _id } = req.user
-    const { vid, total} = req.body
+    const { vid, total } = req.body
 
     try {
         const voucher = await Voucher.findById(vid).select('name _id minPurchaseAmount discount maxDiscountAmount usedCount usedBy maxUsage startDay endDay');;
@@ -282,6 +312,122 @@ const checkVoucher = asyncHandler(async (req, res) => {
         });
     }
 })
+
+const sendVoucherToUser = asyncHandler(async (req, res) => {
+    const { voucherId, email } = req.body;
+    let name = email
+    if (!voucherId || !email) {
+        return res.status(400).json({
+            success: false,
+            message: 'Voucher ID and email are required'
+        });
+    }
+    const user = await User.findOne({ email });
+    if (user)
+        name = user.name;
+
+    try {
+        const voucher = await Voucher.findById(voucherId);
+
+        if (!voucher) {
+            return res.status(404).json({
+                success: false,
+                message: 'Voucher not found'
+            });
+        }
+
+        const formattedStartDay = moment(voucher.startDay).format('DD/MM/YYYY');
+        const formattedEndDay = moment(voucher.endDay).format('DD/MM/YYYY');
+
+        // Tạo nội dung HTML cho email
+        const html = `
+<html>
+<head>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            color: #333;
+        }
+        .email-container {
+            max-width: 600px;
+            margin: auto;
+            background: #f9f9f9;
+            padding: 20px;
+            border-radius: 8px;
+            border: 1px solid #ddd;
+        }
+        h1 {
+            color: #4A90E2;
+            text-align: center;
+        }
+        p {
+            margin: 10px 0;
+            font-size: 16px;
+        }
+        .highlight {
+            color: #FF6F61;
+        }
+        .info {
+            background: #eef;
+            padding: 10px;
+            border-left: 3px solid #4A90E2;
+            margin: 20px 0;
+        }
+        .footer {
+            text-align: center;
+            margin-top: 20px;
+            font-size: 14px;
+            color: #777;
+        }
+        img {
+            display: block;
+            margin: 10px auto;
+            max-width: 100%;
+            height: auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="email-container">
+        <h1>Thông Tin Voucher</h1>
+         <p><strong>Mã giảm giá:</strong> <span class="highlight">${voucher._id}</span></p>
+        <p><strong>Tên:</strong> <span class="highlight">${voucher.name}</span></p>
+        <p><strong>Mức giảm giá:</strong> <span class="highlight">${voucher.discount}%</span></p>
+        <p><strong>Giảm giá tối đa:</strong> <span class="highlight">${voucher.maxDiscountAmount} VND</span></p>
+        <p><strong>Cho đơn từ:</strong> <span class="highlight">${voucher.minPurchaseAmount} VND</span></p>
+        <div class="info">
+            <p><strong>Ngày bắt đầu:</strong> <span class="highlight">${formattedStartDay}</span></p>
+            <p><strong>Ngày kết thúc:</strong> <span class="highlight">${formattedEndDay}</span></p>
+        </div>
+        <div class="footer">
+            Cảm ơn bạn đã sử dụng dịch vụ của chúng tôi!
+        </div>
+    </div>
+</body>
+</html>
+`;
+
+        // Gửi email
+        await sendMail({
+            email: email,
+            subject: `CosmeticsShop xin trân trọng gửi đến quý khách hàng ${name} voucher khuyến mãi`,
+            html: html
+        });
+
+        return res.status(200).json({
+            success: true,
+            message: 'Voucher sent successfully to ' + email
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: 'Error retrieving voucher: ' + error.message
+        });
+    }
+});
+
 module.exports = {
     createVoucher,
     getAllVouchers,
@@ -289,5 +435,6 @@ module.exports = {
     deleteVoucher,
     findByName,
     findById,
-    checkVoucher
+    checkVoucher,
+    sendVoucherToUser
 }
--- server/routes/voucher.js
+++ server/routes/voucher.js
@@ -17,4 +17,6 @@ router.post('/search', verifyAccessToken, ctrls.findByName)
 router.delete('/:vid', [verifyAccessToken, isAdmin], ctrls.deleteVoucher)
 
 router.post('/check', verifyAccessToken, ctrls.checkVoucher)
+
+router.post('/send', [verifyAccessToken, isAdmin], ctrls.sendVoucherToUser)
 module.exports = router
